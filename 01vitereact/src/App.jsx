2ff7391a938f8fb5bb52a78f3af0d391edeb870b
Surya Kiran <surya7386730@gmail.com>
2025-03-02 19:27:34 +0530

    React created through Vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Test from "./test"
+
+function App() {
+
+  return (
+    <Test />
+  )
+}
+
+export default App
---
+++ 01vitereact/src/test.jsx
@@ -0,0 +1,7 @@
+function Test() {
+    return (
+        <h2>This is test</h2>
+    )
+}
+
+export default Test
