c8b42640de4f179600e2f3b305eb98de9d749428
suryakiran18239 <92254802+suryakiran18239@users.noreply.github.com>
2025-05-26 02:24:46 +0000

    fix: correct form submission in TaskForm

--- Taskmanagement/src/components/TaskForm.jsx
+++ Taskmanagement/src/components/TaskForm.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TaskForm.css";
 import Tag from "./Tag";
 
 function TaskForm() {
+  const [taskData, setTaskData] = useState({
+    task: "",
+    status: "todo",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setTaskData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(taskData);
+  };
+
+  /*  const [task, setTask] = useState("");
+  const [status, setStatus] = useState("todo");
+
+  const handleTaskChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleStatusChange = (e) => {
+    setStatus(e.target.value);
+  };
+
+  console.log(task, status);
+*/
+
   return (
     <header className="app_header">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           className="task_input"
           placeholder="Enter your task"
+          name="task"
+          onChange={handleChange}
         />
 
         <div className="task_form_bottom_line">
@@ -20,7 +51,11 @@ function TaskForm() {
             <Tag tagName="React.js" />
           </div>
           <div>
-            <select className="task_status">
+            <select
+              className="task_status"
+              name="status"
+              onChange={handleChange}
+            >
               <option value="todo">Todo</option>
               <option value="doing">Doing</option>
               <option value="done">Done</option>
